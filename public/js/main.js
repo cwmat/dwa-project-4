885a993d127e30079a8568f1546a3ddb26c04c69
cwmateer <cwmateer@ncsu.edu>
2015-12-12 15:20:31 -0500

    bug: Fix textare highlighting when focused.

--- public/js/main.js
+++ public/js/main.js
@@ -22,13 +22,27 @@ $(function() {
     $flashDiv.hide();
   }, 3500);
 
-  // Hide the flash message if the user hovers over the div
-  // $flashDiv.hover(function() {
-  //   $flashDiv.css('opacity', 0.0);
-  //
-  //   setTimeout(function() {
-  //     $flashDiv.hide();
-  //   }, 1000);
-  // });
+  var $trumbowygEditor = $('.trumbowyg-editor');
+  var $trumbowygBox = $('.trumbowyg-box');
+
+  $trumbowygBox.focus(function() {
+    $trumbowygBox.css('border-color', '#66afe9');
+    $trumbowygBox.css('outline', 0);
+    $trumbowygBox.css('box-shadow', 'inset 0 1px 1px rgba(0,0,0,.075),0 0 8px rgba(102,175,233,.6)');
+  });
+
+  $trumbowygBox.blur(function() {
+    $trumbowygBox.css('border-color', '#ddd');
+    $trumbowygBox.css('outline', 1);
+    $trumbowygBox.css('box-shadow', 'inset 0 0 0 rgba(0,0,0,0.0),0 0 0 rgba(0,0,0,0.0)');
+  });
+
+  $trumbowygEditor.focus(function() {
+    $trumbowygBox.triggerHandler('focus');
+  });
+
+  $trumbowygEditor.blur(function() {
+    $trumbowygBox.triggerHandler('blur');
+  });
 
 });
